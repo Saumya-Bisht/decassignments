fe36acfca0db09614846adba71b7a66ff496e291
Saumya-Bisht <70054252+Saumya-Bisht@users.noreply.github.com>
2022-01-02 13:59:24 +0530

    Add files via upload

---
+++ 21dec/min.js
@@ -0,0 +1,16 @@
+var a = prompt("Enter number 1");
+var b = prompt("Enter number 2");
+var c = prompt("Enter number 3");
+if(a > b) {
+    if(a>c){
+  	alert("a is greatest");
+} 
+}
+else if(b > a) {
+    if(b>c){
+  	alert("b is greatest");
+} 
+}
+else {
+  	alert("c is greatest");
+} 
---
+++ 21dec/vowel.js
@@ -0,0 +1,25 @@
+var char = "A";
+
+if(char == "a") {
+ alert("It is a vowel");
+} else if(char == "e") {
+ alert("It is a vowel");
+} else if(char == "i") {
+ alert("It is a vowel");
+} else if(char == "o") {
+ alert("It is a vowel");
+} else if(char == "u") {
+ alert("It is a vowel");
+} else if(char == "A") {
+ alert("It is a vowel");
+} else if(char == "E") {
+ alert("It is a vowel");
+} else if(char == "I") {
+ alert("It is a vowel");
+} else if(char == "O") {
+ alert("It is a vowel");
+} else if(char == "U") {
+ alert("It is a vowel");
+}else {
+ alert("It is a consonant");
+}
