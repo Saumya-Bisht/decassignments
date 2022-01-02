5e0837b59b801523a6679b5e6bf6cb0c6c4bcd49
Saumya-Bisht <70054252+Saumya-Bisht@users.noreply.github.com>
2022-01-02 13:58:28 +0530

    Add files via upload

---
+++ 20dec/max.js
@@ -0,0 +1,19 @@
+var a = 200;
+var b=40;
+var c=90;
+
+if(a > b) {
+    if(a>c){
+  	console.log("a is greatest");
+} 
+}
+else if(b > a) {
+    if(b>c){
+  	console.log("b is greatest");
+} 
+}
+else {
+  	console.log("c is greatest");
+} 
+
+
